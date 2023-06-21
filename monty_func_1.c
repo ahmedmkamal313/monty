6ab1dfc00d3e1ba29eb2688b289ff9ebf66f4e9e
Magdalina1 <majdalol95@gmail.com>
2023-06-21 05:37:45 -0700

    _mul

--- monty_func_1.c
+++ monty_func_1.c
@@ -33,3 +33,30 @@ void _sub(stack_t **stack, unsigned int line_number)
 
 	(*stack)->n = sub;
 }
+
+/**
+ * _mul - mul top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int aux;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		aux = (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n *= aux;
+	}
+}
+
