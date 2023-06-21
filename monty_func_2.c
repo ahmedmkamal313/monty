9baace1f63e879b82ad789c3a6397bbfdb1db5dc
Magdalina1 <majdalol95@gmail.com>
2023-06-21 06:00:51 -0700

    _isalpha

--- monty_func_2.c
+++ monty_func_2.c
@@ -100,3 +100,16 @@ void _pchar(stack_t **stack, unsigned int line_number)
 	putchar(val);
 	putchar('\n');
 }
+
+/**
+ * _isalpha - checks if int is in alphabet
+ * @c: int
+ * Return: 1 if yes, 0 if no
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
