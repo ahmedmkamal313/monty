5c619db736e9931a50b4e3d4a076a3e15adb464f
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-06-21 06:47:59 -0700

    added new 2 functions

--- monty_func_4.c
+++ monty_func_4.c
@@ -1,6 +1,38 @@
 #include "monty.h"
 
 /**
+ * _mod - mod top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	int mod = 0;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		mod = (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n %= mod;
+	}
+}
+/**
  * _sub - sub top of the sack
  * @stack: pointer to monty stack
  * @line_number: number of line opcode occurs on
@@ -30,6 +62,38 @@ void _sub(stack_t **stack, unsigned int line_number)
 	(*stack)->n = sub;
 }
 
+/**
+ * _div - div top of stack
+ * @stack: pointer to list
+ * @line_number: number of line opcode occurs
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int div = 0;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free(var_global.buffer);
+		fclose(var_global.file);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		div = (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n /= div;
+	}
+}
 /**
  * _mul - mul top of stack y second top stack
  * @stack: pointer to lists for monty stack
