d19c0a563481f5c8cda75541d595eddf0d3809ff
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-06-21 06:12:21 -0700

    added new 2 functions to file monty_func_0.c

--- monty_func_0.c
+++ monty_func_0.c
@@ -1,4 +1,62 @@
 #include "monty.h"
+/**
+ * isnumber - check if a string is a number
+ * @str: string to be checked.
+ * Return: returns 1 if is number, or 0 if not.
+ */
+int isnumber(char *str)
+{
+	unsigned int i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[0] == '-')
+		{
+			i++;
+			continue;
+		}
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * parse_line - parses a line for an opcode and arguments
+ * @line: the line to be parsed
+ * @stack: pointer to the head of the stack
+ * @line_number: the current line number
+ * Return: returns the opcode or null on failure
+ */
+char *parse_line(char *line, stack_t **stack, unsigned int line_number)
+{
+	char *op_code, *push, *arg;
+	(void)stack;
+
+	push = "push";
+	op_code = strtok(line, "\n ");
+	if (op_code == NULL)
+		return (NULL);
+
+	if (strcmp(op_code, push) == 0)
+	{
+		arg = strtok(NULL, "\n ");
+		if (isnumber(arg) == 1 && arg != NULL)
+		{
+			var_global.push_arg = atoi(arg);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (op_code);
+}
 
 /**
  * read_file - read bytecode and runs command.
@@ -79,3 +137,5 @@ instruct_func get_op_func(char *str)
 
 	return (instruct[i].f);
 }
+
+
